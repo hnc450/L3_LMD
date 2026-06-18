9ecbe314966396c5436334b317f977befad2fa47
henock <henoctumonakiese@gmail.com>
2026-06-18 21:57:11 +0100

    docs(mcd): Ajout du mcd et modification de la table article pour les articles

--- api/database/migrations/2026_06_17_201204_create_articles_table.php
+++ api/database/migrations/2026_06_17_201204_create_articles_table.php
@@ -13,12 +13,19 @@ return new class extends Migration
     {
          Schema::create('articles', function (Blueprint $table) {
              $table->id();
+             $table
+               ->foreignId('user_id')
+               ->constrained('users')
+               ->onDelete('cascade');
              $table->string('name'); 
              $table->text('description')->nullable();
              $table->decimal('price', 10, 2); 
              $table->integer('stock')->default(0); 
              $table->string('photo')->nullable(); 
-             $table->foreignId('category_id')->constrained()->onDelete('cascade'); 
+             $table
+               ->foreignId('category_id')
+               ->constrained()
+               ->onDelete('cascade'); 
              $table->timestamps();
          });
 
